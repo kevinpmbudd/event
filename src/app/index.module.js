48e1be86f04588ac1732b0080c5ed9da02deac16
Kevin Budd <kevin@kevinbudd.com>
2016-03-07 11:05:39 -0600

    init commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('event', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'toastr']);
+
+})();
